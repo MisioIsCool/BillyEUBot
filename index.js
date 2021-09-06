0805a38c6c3bc92b08a925a0985ae2b188b47f67
MisioIsCool <Anotherguyunderscore@gmail.com>
2021-09-06 18:30:53 +0200

    fds

---
+++ index.js
@@ -0,0 +1,36 @@
+
+                (async()=>{
+                const Discord = require("discord.js");
+                const Database = require("easy-json-database");
+                const devMode = typeof __E_IS_DEV !== "undefined" && __E_IS_DEV;
+                const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+                const s4d = {
+                    Discord,
+                    database: new Database(`${devMode ? S4D_NATIVE_GET_PATH : "."}/db.json`),
+                    joiningMember:null,
+                    reply:null,
+                    tokenInvalid:false,
+                    tokenError: null,
+                    checkMessageExists() {
+                        if (!s4d.client) throw new Error('You cannot perform message operations without a Discord.js client')
+                        if (!s4d.client.readyTimestamp) throw new Error('You cannot perform message operations while the bot is not connected to the Discord API')
+                    }
+                };
+                s4d.client = new s4d.Discord.Client({
+                    intents: [Object.values(s4d.Discord.Intents.FLAGS).reduce((acc, p) => acc | p, 0)],
+                    partials: ["REACTION"]
+                });
+
+                s4d.client.on('ready', async () => {
+  if ((s4dmessage.content) == '!ping') {
+    s4dmessage.channel.send(String('pong'));
+  }
+
+});
+
+await s4d.client.login('ODg0NDY2NzE5NzQ4NDAzMzMw.YTY50g.Jd4W3iP-8uGwTOrRvEwnc9n5F3Q').catch((e) => { s4d.tokenInvalid = true; s4d.tokenError = e; });
+
+
+                return s4d;
+                })();
+            
